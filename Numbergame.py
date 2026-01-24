516e80443c673b2578ae486f23a1e0f026694498
aa2673 <aa2673@kent.ac.uk>
2026-01-24 22:09:53 +0000

    numbergame

--- Numbergame.py
+++ Numbergame.py
@@ -1,12 +1,15 @@
+
 import random
 def main():
     guess = int(input("Guess what number I'm thinking of between 1 and 10"))
     secretnumber = random.randint(1,10)
-    if guess > secretnumber:
-        print("Too high!") 
+    if guess == secretnumber:
+        print("You got it!")
+    elif guess > secretnumber:
+        print("Too high!")
     elif guess < secretnumber:
         print("Too low!")
     else:
-        print("You got it!")
+        print("Enter a valid guess please")
 
 main()
---
+++ numbergame.py
@@ -0,0 +1,23 @@
+import random
+def main():
+    secretnumber = random.randint(1,10)
+    while True:
+        try:
+            guess = int(input("Guess what number I'm thinking of between 1 and 10:"))
+        except ValueError:
+            print("Please enter your guess in numerical form")
+            continue
+        
+        if guess > 0 and guess < 10:
+            if guess == secretnumber:
+                print("You got it!")
+                break
+            elif guess > secretnumber:
+                print("Too high!")
+            else:
+                print("Too low!")
+       
+        else:
+            print("I said between 1 and 10!")
+
+main()
